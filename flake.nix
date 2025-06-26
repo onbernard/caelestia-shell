88a29c88d47a541df45e1584bcfbd06df5566c19
Onésime BERNARD <bernard.onesime@gmail.com>
2025-06-26 12:55:50 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,107 @@
+{
+  description = "caelestia-shell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    caelestia-cli = {
+      url = "github:onbernard/caelestia-cli";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {self, ...}:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlay = [];
+        };
+        enhanced-beat-detector = pkgs.stdenv.mkDerivation {
+          pname = "enhanced-beat-detector";
+          version = "1.0";
+
+          src = ./assets;
+
+          buildInputs = with pkgs; [
+            pipewire
+            aubio
+          ];
+
+          nativeBuildInputs = with pkgs; [pkg-config];
+
+          CXXFLAGS = [
+            "-std=c++17"
+            "-O3"
+            "-Wall"
+            "-Wextra"
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            g++ $CXXFLAGS \
+                -I${pkgs.pipewire.dev}/include/pipewire-0.3 \
+                -I${pkgs.pipewire.dev}/include/spa-0.2 \
+                -I${pkgs.aubio}/include/aubio \
+                -L${pkgs.pipewire}/lib \
+                -L${pkgs.aubio}/lib \
+                -lpipewire-0.3 -laubio -pthread \
+                -o $out/bin/beat_detector \
+                $src/beat_detector.cpp
+          '';
+        };
+        caelestia-shell = pkgs.stdenv.mkDerivation {
+          pname = "caelestia-shell";
+          version = "1.0";
+
+          propagatedBuildInputs = with pkgs; [
+            inputs.quickshell.packages.${system}.default
+            inputs.caelestia-cli.packages.${system}.caelestia-cli
+            enhanced-beat-detector
+            papirus-icon-theme
+            dbus
+            ddcutil # Monitor settings
+            brightnessctl # Monitor settings
+            app2unit # Applications desktop entries
+            cava # Audio visualizer
+            curl # For api calls like weather stuff
+            bluze # Bluetooth
+            lm_sensors # System usage
+            # For scripts
+            bash
+            fishMinimal
+            coreutils
+            findutils
+            gawk
+          ];
+
+          src = ./.;
+
+          installPhase = ''
+            cp -r assets $out/
+            cp -r config $out/
+            cp -r modules $out/
+            cp -r services $out/
+            cp -r utils $out/
+            cp -r widgets $out/
+            cp shell.qml $out/
+          '';
+        };
+      in {
+        packages = {
+          default = caelestia-shell;
+          caelestia-shell = caelestia-shell;
+          enhanced-beat-detector = enhanced-beat-detector;
+        };
+        devShell = pkgs.mkShell {
+          packages = [];
+          shellHook = ''
+            echo "uwu"
+          '';
+        };
+      });
+}
